2b806da3cfd7080500c054767954e54a505db4be
abdo <xitaber535@hmnmw.com>
2020-12-15 13:52:41 +0100

    Update Part 11-HPC Microservices "min= 24min"

--- sec-service/src/main/java/org/sid/secservice/security/SecurityConfig.java
+++ sec-service/src/main/java/org/sid/secservice/security/SecurityConfig.java
@@ -1,23 +1,68 @@
 package org.sid.secservice.security;
 
+import org.sid.secservice.security.entities.AppUser;
+import org.sid.secservice.security.filters.JwtAuthenticationFilter;
+import org.sid.secservice.security.filters.JwtAuthorizationFilter;
+import org.sid.secservice.security.service.AccountService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private AccountService accountService;
+
+    public SecurityConfig(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                AppUser appUser=accountService.loadUserByUsername(username);
+                Collection<GrantedAuthority>authorities=new ArrayList<>();
+                appUser.getAppRoles().forEach(p->{
+                    authorities.add(new SimpleGrantedAuthority(p.getRoleName()));
+                });
+                return new User(appUser.getUsername(), appUser.getPassword(), authorities);
+            }
+        });
 
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.headers().frameOptions().disable();
-       http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        //http.formLogin();
+       http.authorizeRequests().anyRequest().authenticated();
+       http.addFilter(new JwtAuthenticationFilter( authenticationManagerBean()));
+       http.addFilterBefore(new JwtAuthorizationFilter(),UsernamePasswordAuthenticationFilter.class);
     }
 
-
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
