e69f9761a42324f4af6720f81595134fa68df6fc
Abdelbasset waraki <waraki.abdelbasset@emsi-edu.ma>
2021-01-16 13:23:54 +0100

    Delete products-app-kc directory

--- products-app-kc/src/main/java/ma/enset/productsapp/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.enset.productsapp.repositories;
-
-import ma.enset.productsapp.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-
-}
